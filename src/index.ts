ebf515b2dd853f38867004114772af7a1dc33442
Ahmed ALAbsi <alabsi91@gmail.com>
2023-12-26 18:59:49 +0300

    Fix attempt 2 for react-native import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import { ReturnSearchInfo } from './types';
+
 export { go, single } from './fuzzy';
 export { highlight } from './highlight';
 export { charactersMap } from './store';
 export { transformationFn } from './utils';
 
 export * from './types';
+
+/**
+ * - For `HTML` import `highlight` from `'fuzzy-search'`
+ * - For `React` import `HighlightReact` from `'fuzzy-search/react'`
+ * - For `React-Native` import `HighlightReact` from `'fuzzy-search'`
+ * @param result - Search result object
+ * @param HighlightedTextCP - Highlighted text component
+ * @param RegularTextCP - Regular text component
+ * @returns - Returns an array of JSX elements
+ */
+export declare function HighlightReact(
+  result: ReturnSearchInfo<string | object>,
+  HighlightedTextCP: React.FC<{
+    text: string;
+  }>,
+  RegularTextCP?: React.FC<{
+    text: string;
+  }>
+): React.JSX.Element[];
